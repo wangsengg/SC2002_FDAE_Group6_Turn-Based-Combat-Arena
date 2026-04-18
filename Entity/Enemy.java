c009446358ba35612bee3692a962de2bc848db36
Toh Wang Seng <wangsengtoh421@gmail.com>
2026-04-18 15:09:07 +0800

    Delete Entity/Enemy.java

--- Entity/Enemy.java
+++
@@ -1,7 +0,0 @@
-package Game.Entity;
-
-public abstract class Enemy extends Combatant{
-    public Enemy(String name, int maxHP, int attack, int defense, int speed){
-        super(name, maxHP, attack, defense, speed);
-    }
-}
