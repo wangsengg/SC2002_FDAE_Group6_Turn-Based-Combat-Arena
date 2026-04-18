46241de26568d2efc786977473b1cb32c8851d6d
Toh Wang Seng <wangsengtoh421@gmail.com>
2026-04-18 15:09:44 +0800

    Fix package structure and reorganize project files

--- Entity/Warrior.java
+++
@@ -1,9 +0,0 @@
-package Game.Entity;
-
-import Game.SpecialSkill.ShieldBash;
-
-public final class Warrior extends Player{
-    public Warrior(){
-        super("Warrior", 260, 40, 20, 30, new ShieldBash());
-    }
-}
